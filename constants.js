2025e165316d10b75b0fc178c5cba4a5566bcea4
freerangeandy <freerangeandy@gmail.com>
2020-11-18 15:09:51 -0500

    use parcel to allow importing between JS files

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
-const CANVAS_WIDTH = 1200
-const CANVAS_HEIGHT = 600
-const BOX_WIDTH = 20
+export const CANVAS_WIDTH = 1200
+export const CANVAS_HEIGHT = 600
+export const BOX_WIDTH = 20
 
-const maxRow = CANVAS_HEIGHT/BOX_WIDTH
-const maxCol = CANVAS_WIDTH/BOX_WIDTH
+export const maxRow = CANVAS_HEIGHT/BOX_WIDTH
+export const maxCol = CANVAS_WIDTH/BOX_WIDTH
