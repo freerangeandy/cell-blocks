4d62c086cc173b3a5866f1abb72dbce4fcb4f9eb
freerangeandy <freerangeandy@gmail.com>
2020-11-30 16:42:05 -0500

    bug fix: back out maxRow, maxCol change (Cell class imports constants)

--- src/js/constants.js
+++ src/js/constants.js
@@ -2,6 +2,9 @@ export const CANVAS_WIDTH = 1200
 export const CANVAS_HEIGHT = 600
 export const BOX_WIDTH = 20
 
+export const maxRow = CANVAS_HEIGHT/BOX_WIDTH
+export const maxCol = CANVAS_WIDTH/BOX_WIDTH
+
 export const RAND_DENSITY = 0.4
 
 export const GLIDER_PATTERN = [[0,1,0],[0,0,1],[1,1,1]]
